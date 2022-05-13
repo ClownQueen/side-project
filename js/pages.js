f3afae8a43d3c24ca43fa8e8e84b9405319f617f
Micah Wood <micahswood7@gmail.com>
2022-05-13 16:44:04 -0500

    added another image and pages.js, also edited style.css and Home-page.html

---
+++ js/pages.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function changePic(){
+    document.body.style.backgroundImage = "url('/img/delapitadeHome.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100% 100%";
+    return `<button id="exiting" onclick="exitPic()" style="color: transparent; background-color: transparent; border: transparent; cursor: pointer;">Exit the home</button>
+     <button id="venture" onclick="anotherPic()" style="color: transparent; background-color: transparent; border: transparent; cursor: pointer;">Venture the home further</button>`
+}
+
+function exitPic() {
+    document.body.style.backgroundImage = "url('/img/Oldhouse.png')"
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "100% 100%"
+    return `<h3 id="endingOne">You Can Never Enter Again</h3>
+     <p id="endingOnePlot">You exit the house as the sinking feeling never leaves you. Never venturing further you don't know what could've been lying in the house leaving the home to be a mystery. </p>`
+}
+
+function anotherPic(){
+    document.body.style.backgroundImage = "url('/img/oldLibrary.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100% 100%";
+}
