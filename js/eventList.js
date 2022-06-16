9373895a4a797b5f8a340ed778bd3a6510f7b056
Micah Wood <micahswood7@gmail.com>
2022-06-15 23:27:10 -0500

    added function to eventListener as well as fixed pages

--- js/eventList.js
+++ js/eventList.js
@@ -1,2 +1,10 @@
 "use strict";
 
+function startGameOption(){
+    if (startGame() === true){
+        alert("let's start the adventure then, shall we.")
+    } else {
+        alert("Than go home, because the adventure is not for you!")
+    }
+}
+
--- js/pages.js
+++ js/pages.js
@@ -10,21 +10,6 @@ function changePic(){
 function startGame(){
     $("#start").click(function(){
         changePic();
-        alert("hello traveler I am sure you wanted to explore this abandoned home no?");
+        confirm("hello traveler I am sure you wanted to explore this abandoned home no?");
     });
 }
-// return `<button id="exiting" onclick="exitPic()" style="color: transparent; background-color: transparent; border: transparent; cursor: pointer;">Exit the home</button>
-//      <button id="venture" onclick="anotherPic()" style="color: transparent; background-color: transparent; border: transparent; cursor: pointer;">Venture the home further</button>`
-// function exitPic() {
-//     document.body.style.backgroundImage = "url('/img/Oldhouse.png')"
-//     document.body.style.backgroundRepeat = "no-repeat"
-//     document.body.style.backgroundSize = "100% 100%"
-//     return `<h3 id="endingOne">You Can Never Enter Again</h3>
-//      <p id="endingOnePlot">You exit the house as the sinking feeling never leaves you. Never venturing further you don't know what could've been lying in the house leaving the home to be a mystery. </p>`
-// }
-//
-// function anotherPic(){
-//     document.body.style.backgroundImage = "url('/img/oldLibrary.png')";
-//     document.body.style.backgroundRepeat = "no-repeat";
-//     document.body.style.backgroundSize = "100% 100%";
-// }
