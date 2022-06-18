71ff40bff38ff7a769e3ca8bce432583407021e7
Micah Wood <micahswood7@gmail.com>
2022-06-17 23:42:54 -0500

    adding descriptions to functions and their purpose

--- js/eventList.js
+++ js/eventList.js
@@ -1,5 +1,11 @@
 "use strict";
 
+//new game function allows user to decide if the want to start a new game or not
+function newGame(){
+
+}
+
+//start game function allows a person to decide if the want to explore the house or not
 function startGameOption(){
     if (startGame() === true){
         alert("let's start the adventure then, shall we.")
@@ -9,3 +15,7 @@ function startGameOption(){
     }
 }
 
+//search function will allow the person to look in that certain area for items
+function search (){
+
+}
--- js/pages.js
+++ js/pages.js
@@ -1,18 +1,20 @@
 "use strict";
 
+//changePic function allows the screen to change it's image depending on the room you are in
 function changePic(){
     document.body.style.backgroundImage = "url('/img/delapitadeHome.png')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "100% 100%";
-
 }
 
+//homePic function is allowing the user to go back to the m=home page if desired
 function homePic(){
     document.body.style.backgroundImage = "url('/img/Oldhouse.png')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "100% 100%";
 }
 
+//startGame function allows the user to confirm if they want to start a new game or not
 function startGame(){
     $("#start").click(function(){
         changePic();
