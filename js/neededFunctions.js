c0edf58f3dad63f4334ca948822ee8add6d414f0
Micah Wood <micahswood7@gmail.com>
2022-06-23 21:40:50 -0500

    add todos to the neededFunctions.js

--- js/neededFunctions.js
+++ js/neededFunctions.js
@@ -1,12 +1,14 @@
 "use strict";
 
 //child buttons needed to reach different js page
+/**TODO: MAKE THE BUTTONS AND SPEECH FOR THE DIFFERENT ROOMS APPEAR AND BE ABLE TO PULL THEM FOR THE EVENTLIST.JS*/
 let btn1 = document.createElement("button");
 let btn2 = document.createElement("button");
 let btn3 = document.createElement("button");
 let newRoom = document.createElement("p");
 
 //changePic function allows the screen to change its image depending on the room you are in
+/**TODO: MAKE A NEW ROOM OF THE HOUSE APPEAR AND HAVE THE DIALOG AND BUTTONS APPEAR IN SAID ROOMS*/
 function livingRoom(){
     document.body.style.backgroundImage = "url('/img/delapitadeHome.png')";
     document.body.style.backgroundRepeat = "no-repeat";
@@ -22,6 +24,7 @@ function livingRoom(){
 }
 
 //homePic function is allowing the user to go back to the m=home page if desired
+/**TODO: MAKE IT POSSIBLE TO SHOW THE HOME PAGE IF PLAYER DOESN'T WANT TO START THE GAME*/
 function homePic(){
     document.body.style.backgroundImage = "url('/img/Oldhouse.png')";
     document.body.style.backgroundRepeat = "no-repeat";
@@ -29,6 +32,7 @@ function homePic(){
 }
 
 //startGame function allows the user to confirm if they want to start a new game or not
+/**TODO: MAKE THE STARTGAMEOPTION WORK ONCE THE PERSON HAS PRESSED THE START GAME BUTTON*/
 function startGame(){
     $("#start").click(function(){
         startGameOption();
@@ -36,6 +40,7 @@ function startGame(){
 }
 
 //randomItems generated inside the rooms of the house
+/**TODO: MAKE RANDOM ITEMS APPEAR IN ROOMS AND ONLY HAVE THEM APPEAR ONCE*/
 function randomItem(){
     let items = ["Room Keys", "Broken Camera", "Vase full of dead Flowers", "Picture frames with photos and code on the back", "kids drawings", "lamps", "flashlight"]
     return "you found " + items.random
