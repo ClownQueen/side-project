8d74a21ecd962b38fe61b61f6b1360c99cdc991b
Micah Wood <micahswood7@gmail.com>
2022-09-04 23:15:59 -0500

    added to practice.js

---
+++ js/practice.js
@@ -0,0 +1,5 @@
+"use strict";
+
+/** TODO: look up javascript practice material to get better idea of technices in javascript */
+
+//Write a JavaScript program that accept two integers and display the larger
